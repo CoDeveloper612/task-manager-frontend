7970216fbc996ed066e2e47962079c3393f294cd
codeveloper <michaeljoe612@outlook.com>
2024-11-10 10:32:11 -0600

    App component complete

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import AddTask from './components/AddTask';
-// import TaskList from './components/TaskList';
+import TaskList from './components/TaskList';
 
 const App = () => {
     const [tasks, setTasks] = useState([]);
@@ -10,7 +10,7 @@ const App = () => {
         const response = await axios.get('http://localhost:5000/api/tasks');
         setTasks(response.data);
     };
-    
+
     useEffect(() => {
         fetchTasks();
     }, []);
@@ -19,7 +19,7 @@ const App = () => {
         <div className="container pt-3">
             <h1>Task Manager</h1>
             <AddTask fetchTasks={fetchTasks} />
-            // <TaskList tasks={tasks} fetchTasks={fetchTasks} />
+            <TaskList tasks={tasks} fetchTasks={fetchTasks} />
         </div>
     );
 };
