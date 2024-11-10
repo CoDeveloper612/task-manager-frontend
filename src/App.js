c1bf52b23f7059dc4979d7fb3084e93ec7ea2491
codeveloper <michaeljoe612@outlook.com>
2024-11-10 09:41:01 -0600

    task add form build

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import AddTask from './components/AddTask';
+// import TaskList from './components/TaskList';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    const [tasks, setTasks] = useState([]);
+
+    const fetchTasks = async () => {
+        const response = await axios.get('http://localhost:5000/api/tasks');
+        setTasks(response.data);
+    };
+
+    useEffect(() => {
+        fetchTasks();
+    }, []);
+
+    return (
+        <div className="container pt-3">
+            <h1>Task Manager</h1>
+            <AddTask fetchTasks={fetchTasks} />
+            // <TaskList tasks={tasks} fetchTasks={fetchTasks} />
+        </div>
+    );
+};
 
 export default App;
