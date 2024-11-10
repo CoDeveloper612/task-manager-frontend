d452cbc717a14dd1e0baafb6d8fb2d229b303d0f
codeveloper <michaeljoe612@outlook.com>
2024-11-10 10:31:05 -0600

    TaskList component complete

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -3,7 +3,7 @@ import TaskItem from './TaskItem';
 
 const TaskList = ({ tasks, fetchTasks }) => {
     return (
-        <div>
+        <div className='mt-3'>
             {tasks.map(task => <TaskItem key={task._id} task={task} fetchTasks={fetchTasks} />)}
         </div>
     );
