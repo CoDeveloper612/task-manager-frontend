a16357112c86b680a406d6266388f1935c00e0cc
codeveloper <michaeljoe612@outlook.com>
2024-11-10 09:52:12 -0600

    AddTask component handleSubmit funtion added

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -14,6 +14,7 @@ const AddTask = ({ fetchTasks }) => {
 
     const handleSubmit = async (e) => {
         e.preventDefault();
+        
         await axios.post('http://localhost:5000/api/tasks', { title, description, completed });
         fetchTasks();
         handleInit();
