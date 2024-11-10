834b8f841c8e3858c642bc40446447d6ba0aa8ea
codeveloper <michaeljoe612@outlook.com>
2024-11-10 09:49:45 -0600

    taskItem component build

---
+++ src/components/TaskItem.js
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+import TaskEdit from './TaskEdit';
+
+const TaskItem = ({ task, fetchTasks }) => {
+    const [ edit, setEdit ] = useState(false);
+    const [title, setTitle] = useState(task.title);
+    const [description, setDescription] = useState(task.description);
+    const [completed, setCompleted] = useState(task.completed);
+
+    return (
+        <div>
+                <h3>{title}</h3>
+                <span>{description}</span>
+                
+                <p>{completed ? 'Completed' : 'Pending'}</p>
+                <div class="btn-group btn-group-sm">
+                    <button onClick={() => { setEdit(!edit)}} btn btn-primary>Edit</button>
+                    {completed ? <button onClick={() => updateCompeted(false)}>Mark as Pending</button>
+                    : <button onClick={() => updateCompeted(true)}>Mark as Completed</button>}
+                    <button onClick={handleDelete}>Delete</button>
+                </div>
+            
+            <TaskEdit edit={edit} handleEdit={handleEdit} handleUpdate={handleUpdate} task={task} />
+        </div>
+    );
+};
+
+export default TaskItem;
